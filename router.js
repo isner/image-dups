79e252f195fba74360acf60da2d948b8a5b59a41
Matt Isner <matthew.isner@deque.com>
2014-01-19 14:01:56 -0500

    [update] router.js (renames 'handles' object, and adds logging)

--- router.js
+++ router.js
@@ -1,12 +1,13 @@
 
 exports.route = route;
 
-function route(handle, pathname, request, response) {
-  if (typeof handle[pathname] === 'function') {
-    handle[pathname](request, response);
+function route(handles, pathname, request, response) {
+  if (typeof handles[pathname] === 'function') {
+    console.log('handling a request for ' + pathname);
+    handles[pathname](request, response);
 
   } else {
-    console.log('No request handler found for ' + pathname);
+    console.log('no request handler found for ' + pathname);
     response.writeHead(404);
     response.end();
   }
