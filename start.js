81b38ccc63699736224bf433418b6d4e8f797ec2
Matt Isner <matthew.isner@deque.com>
2014-01-19 14:00:41 -0500

    [updates] start.js (adds a handle for requests to results.text)

--- start.js
+++ start.js
@@ -8,6 +8,7 @@ var handles = {};
 handles['/'] = handlers.scan;
 handles['/scan'] = handlers.scan;
 handles['/results'] = handlers.results;
+handles['/results.txt'] = handlers.resultsAdd;
 handles['/favicon.ico'] = handlers.favicon;
 
 server.start(router.route, handles);
